62f82f75347ceba7f9f1c4a5c8113c678e416799
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-11-12 09:33:44 +0200

    Implementation with do...while

--- LoopStatementsWhile/LoopStatementsWhile/Program.cs
+++ LoopStatementsWhile/LoopStatementsWhile/Program.cs
@@ -4,7 +4,16 @@ namespace LoopStatementsWhile
 {
     class Program
     {
+        
         static void Main(string[] args)
+        {
+            ImplementationWithDoWhile();
+        }
+
+        /// <summary>
+        /// Implements the autentication with a while loop.
+        /// </summary>
+        static void ImplementationWihWhile()
         {
             string secretUser = "admin";
             string secretPassword = "12345";
@@ -29,6 +38,35 @@ namespace LoopStatementsWhile
                     Console.WriteLine($"Username or password mismatched, please try again");
                 }
             }
+        }
+
+        static void ImplementationWithDoWhile()
+        {
+            string secretUser = "admin";
+            string secretPassword = "12345";
+
+            Console.WriteLine("Hello, please authenticate first!");
+            bool isAuthenticated = false;
+            do
+            {
+                Console.Write("User=");
+                string user = Console.ReadLine();
+
+                Console.Write("Password=");
+                string password = Console.ReadLine();
+
+                if (string.Equals(user, secretUser, StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(password, secretPassword, StringComparison.Ordinal))
+                {
+                    Console.WriteLine($"Wellcome {user}!");
+                    isAuthenticated = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Username or password mismatched, please try again");
+                }
+            }
+            while (!isAuthenticated);
         }
     }
 }
