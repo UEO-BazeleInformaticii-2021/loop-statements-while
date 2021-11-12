7e6dd54f01000ce309121bd2ae7ed1dd536d9dbe
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-11-12 08:37:58 +0200

    Initial implementation

---
+++ LoopStatementsWhile/LoopStatementsWhile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoopStatementsWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretUser = "admin";
+            string secretPassword = "12345";
+
+            Console.WriteLine("Hello, please authenticate first!");
+            bool isAuthenticated = false;
+            while (!isAuthenticated)
+            {
+                Console.Write("User=");
+                string user = Console.ReadLine();
+
+                Console.Write("Password=");
+                string password = Console.ReadLine();
+
+                if (string.Equals(user, secretUser, StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(password, secretPassword, StringComparison.Ordinal))
+                {
+                    isAuthenticated = true;
+                    Console.WriteLine($"Wellcome {user}!");
+                }
+                else
+                {
+                    Console.WriteLine($"Username or password mismatched, please try again");
+                }
+            }
+        }
+    }
+}
